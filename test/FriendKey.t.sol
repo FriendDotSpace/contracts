aae8c260229192c3f789eeeb5b1f44a6ed741ac3
Viki Val <viktorko99@gmail.com>
2025-06-11 23:12:58 +0200

    :test_tube: tokenUri

--- test/FriendKey.t.sol
+++ test/FriendKey.t.sol
@@ -5,6 +5,7 @@ import {Test, console} from "forge-std/Test.sol";
 import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 import {FriendKey} from "src/FriendKey.sol";
 import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 
 // Simple Mock ERC20 for testing purposes
 contract MockERC20 is IERC20Metadata {
@@ -64,6 +65,8 @@ contract MockERC20 is IERC20Metadata {
 contract FriendKeyTest is Test {
     FriendKey public instance;
     MockERC20 public mockUsdc;
+
+    using Strings for uint256;
 
     address public owner;
     address public devFeeDestination;
@@ -224,6 +227,20 @@ contract FriendKeyTest is Test {
 
         // Verify USDC balance increased (received funds from sale)
         assertTrue(balanceAfter > balanceBefore, "Balance should increase after selling");
+    }
+
+    function testUri() public {
+        string memory myLittleUri = "http://localhost:3001/api/metadata/";
+        
+        // Set the URI (assuming the owner has permission to do this)
+        vm.startPrank(owner);
+        instance.setURI(myLittleUri);
+        vm.stopPrank();
+        
+        // Verify the URI for the token
+        string memory retrievedUri = instance.uri(CREATOR_TOKEN_ID);
+        string memory expectedUri = string.concat(myLittleUri, CREATOR_TOKEN_ID.toString());
+        assertEq(retrievedUri, expectedUri, "URI does not match expected value");
     }
 
     function testSellMultipleShares() public {
