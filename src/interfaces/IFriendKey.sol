8867eda7b5c63351ff552f84f2b38bb32c9f1e05
Viki Val <viktorko99@gmail.com>
2025-07-11 17:08:58 +0200

    :nail_care: update IFriendKey interface to include bondingToken function

--- src/interfaces/IFriendKey.sol
+++ src/interfaces/IFriendKey.sol
@@ -2,15 +2,18 @@
 // OpenZeppelin Contracts (last updated v5.1.0) (token/ERC20/IERC20.sol)
 
 pragma solidity ^0.8.27;
+
 import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+// import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 /**
  * @dev Minimal Interface of the FriendKey.
  */
 interface IFriendKey is IERC1155 {
-
     /**
      * @dev Returns the value of tokens owned by `account`.
      */
     function creatorByTokenId(uint256 tokenId) external view returns (address);
+
+    function bondingToken() external view returns (address);
 }
