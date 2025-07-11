8fa2e40a73449048a89f08af71cb56dd753f5c8b
Viki Val <viktorko99@gmail.com>
2025-07-11 17:34:08 +0200

    :zap: refactor mint function to conditionally enforce minting limit for non-owners

--- src/FriendUSD.sol
+++ src/FriendUSD.sol
@@ -14,7 +14,10 @@ contract FriendUSD is ERC20, ERC20Burnable, Ownable, ERC20Permit {
     }
 
     function mint(address to, uint256 amount) public {
-        require(amount <= 100 * 10 ** decimals(), "FriendUSD: Cannot mint more than 100 tokens at once");
+        if (msg.sender != owner()) {
+            require(amount <= 100 * 10 ** decimals(), "FriendUSD: Cannot mint more than 100 tokens at once");
+        }
+        
         _mint(to, amount);
     }
 }
