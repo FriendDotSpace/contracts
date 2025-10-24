89aebdec54f2e5a3fdeee64f15eb3db21aab0bc8
Dávid <david.barbora@vacuumlabs.com>
2025-10-24 10:17:16 +0200

    fmt fix

--- script/FriendKey.s.sol
+++ script/FriendKey.s.sol
@@ -23,7 +23,7 @@ contract FriendKeyScript is Script {
         address tradingPoolFeeDestination = initialOwner;
         address devFeeDestination = initialOwner;
         address usdc = 0xC2d95a27116A694565eb14c14A2ae332FFF54e0A;
-        address constant AUTHORITY = 0xe18b241E97793C05d7dF05d0C1a3Dec8ac08586D;
+        address AUTHORITY = 0xe18b241E97793C05d7dF05d0C1a3Dec8ac08586D;
 
         // Deploy FriendStake beacon
         address friendStakeBeacon = Upgrades.deployBeacon("FriendStake.sol", initialOwner);
