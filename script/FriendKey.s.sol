bd5245a232a39ec38d7486054cee8069267aa37a
Dávid <david.barbora@vacuumlabs.com>
2025-10-24 09:59:09 +0200

    remove unused script

--- script/FriendKey.s.sol
+++ script/FriendKey.s.sol
@@ -23,6 +23,7 @@ contract FriendKeyScript is Script {
         address tradingPoolFeeDestination = initialOwner;
         address devFeeDestination = initialOwner;
         address usdc = 0xC2d95a27116A694565eb14c14A2ae332FFF54e0A;
+        address constant AUTHORITY = 0xe18b241E97793C05d7dF05d0C1a3Dec8ac08586D;
 
         // Deploy FriendStake beacon
         address friendStakeBeacon = Upgrades.deployBeacon("FriendStake.sol", initialOwner);
@@ -40,7 +41,7 @@ contract FriendKeyScript is Script {
                 CREATOR_PERFORMANCE_FEE_PERCENT,
                 address(usdc),
                 friendStakeBeacon,
-                initialOwner,
+                AUTHORITY,
                 1 days
             )
         );
