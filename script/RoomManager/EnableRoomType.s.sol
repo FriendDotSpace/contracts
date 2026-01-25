2f690f03fc78e2d6c21964424d6c1e34821afac7
askwhyharsh <askwhyharsh@gmail.com>
2026-01-25 16:41:23 +0530

    broadcasts and room manager update for social room limit

--- script/RoomManager/EnableRoomType.s.sol
+++ script/RoomManager/EnableRoomType.s.sol
@@ -12,7 +12,9 @@ import {IFriendKey} from "src/interfaces/IFriendKey.sol";
  */
 contract EnableRoomTypeScript is Script {
     // Update with deployed RoomManager proxy
-    address constant ROOM_MANAGER_PROXY = 0x4a31C071e797d8B818B67a80768e35EE31961C4A; // testnet
+    // address constant ROOM_MANAGER_PROXY = 0x4a31C071e797d8B818B67a80768e35EE31961C4A; // testnet
+    // address constant ROOM_MANAGER_PROXY = 0x85f77d7D29e3f641CCdA8AC47c599A97738041B9; // pre-prod
+    address constant ROOM_MANAGER_PROXY = 0xbF4E9bF4aefBbA62bA1964fD70f69581bA9691d8; // prod
 
     function setUp() public {}
 
--- script/RoomManager/FriendRoomManagerUpgrade.s.sol
+++ script/RoomManager/FriendRoomManagerUpgrade.s.sol
@@ -9,9 +9,9 @@ import {IFriendKey} from "src/interfaces/IFriendKey.sol";
 
 contract FriendRoomManagerUpgradeScript is Script {
     // Update this address based on your deployment
-    address constant ROOM_MANAGER_PROXY = 0x4a31C071e797d8B818B67a80768e35EE31961C4A; // testnet
+    // address constant ROOM_MANAGER_PROXY = 0x4a31C071e797d8B818B67a80768e35EE31961C4A; // testnet
     // address constant ROOM_MANAGER_PROXY = 0x85f77d7D29e3f641CCdA8AC47c599A97738041B9; // pre-prod
-    // address constant ROOM_MANAGER_PROXY = 0x94C36A05D72C75d70eDa968FFa034a018508018A; // prod
+    address constant ROOM_MANAGER_PROXY = 0xbF4E9bF4aefBbA62bA1964fD70f69581bA9691d8; // prod
 
     function setUp() public {}
 
